f4301e4cb5d3acd402746e207efe746ecbc2d4f2
noname <pacomc99@gmail.com>
2021-03-03 16:54:41 +0100

    Commit hola

--- app/src/main/java/com/project/getfit/MainActivity.java
+++ app/src/main/java/com/project/getfit/MainActivity.java
@@ -19,7 +19,7 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-// Cambioooooo
+// Hola
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
