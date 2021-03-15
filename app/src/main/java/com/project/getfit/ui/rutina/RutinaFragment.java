a18072d2e29f3bb756a6c91332f200d73db854f5
atcrosproyect <dangonrey>
2021-03-15 22:24:13 +0100

    Interfaz de rutina.

--- app/src/main/java/com/project/getfit/ui/rutina/RutinaFragment.java
+++ app/src/main/java/com/project/getfit/ui/rutina/RutinaFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,13 @@ public class RutinaFragment extends Fragment {
 
     private RutinaViewModel rutinaViewModel;
 
+    private View root;
+
+    public LinearLayout contenido_principal;
+    public LinearLayout contenido_ejercicio;
+    public LinearLayout contenido_empezar;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rutinaViewModel = new ViewModelProvider(this).get(RutinaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_rutina, container, false);
@@ -30,7 +38,32 @@ public class RutinaFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        definirVariables();
+        reiniciarLinear();
+        contenido_principal.setVisibility(View.VISIBLE);
 
         return root;
+    }
+
+    public void definirVariables() {
+        contenido_principal = root.findViewById(R.id.linearPrincipalRutina);
+        contenido_ejercicio = root.findViewById(R.id.linearEjercicioRutina);
+        contenido_empezar = root.findViewById(R.id.linearEmpezarRutina);
+    }
+
+    public void reiniciarLinear() {
+        contenido_principal.setVisibility(View.GONE);
+        contenido_ejercicio.setVisibility(View.GONE);
+        contenido_empezar.setVisibility(View.GONE);
+    }
+
+    public void irAEjercicios(View v) {
+        reiniciarLinear();
+        contenido_ejercicio.setVisibility(View.VISIBLE);
+    }
+
+    public void irARutina(View v) {
+        reiniciarLinear();
+        contenido_empezar.setVisibility(View.VISIBLE);
     }
 }
