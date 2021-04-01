9b8da25a4e1880a8e126cdf7b91c433a133ef7e7
atcrosproyect <dangonrey>
2021-04-01 13:22:43 +0200

    Guardar eventos en calendario

--- app/src/main/java/com/project/getfit/ui/calendario/CalendarioFragment.java
+++ app/src/main/java/com/project/getfit/ui/calendario/CalendarioFragment.java
@@ -2,16 +2,21 @@ package com.project.getfit.ui.calendario;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -22,23 +27,31 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.project.getfit.R;
+import com.project.getfit.ui.recetas.ListaCalendario;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CalendarioFragment extends Fragment {
 
     private CalendarioViewModel calendarioViewModel;
     private TextView miFecha;
     private Button botonAñadirEvento;
+    private Button botonGuardarEvento;
+    private Button botonCancelarEvento;
     private Button botonHora;
     private LinearLayout linearMostrarEvento;
     private LinearLayout linearAñadirEvento;
     private TextView tituloEvento;
     private TextView descripcionEvento;
     private TextView horaEvento;
-
+    private String diaActual;
+    private String mesActual;
+    private String añoActual;
+    private ListView listViewCalendario;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -59,28 +72,45 @@ public class CalendarioFragment extends Fragment {
         if(diaInt < 10) {dia = "0" + diaInt;}
         if(mesInt < 10) {mes = "0" + mesInt;}
         String fecha = dia + "/" + mes + "/" + año;
+        diaActual = dia;mesActual = mes;añoActual = año.toString();
         miFecha.setText(fecha);
 
         linearMostrarEvento = root.findViewById(R.id.linearMostrarEventos);
         linearAñadirEvento = root.findViewById(R.id.linearAñadirEventos);
-        botonAñadirEvento = root.findViewById(R.id.boton_guardar_evento);
+        botonAñadirEvento = root.findViewById(R.id.boton_añadir_evento);
+        botonGuardarEvento = root.findViewById(R.id.boton_guardar_evento);
+        botonCancelarEvento = root.findViewById(R.id.boton_cancelar_evento);
         botonHora = root.findViewById(R.id.botonSeleccionarHora);
 
         linearMostrarEvento.setVisibility(View.VISIBLE);
         linearAñadirEvento.setVisibility(View.GONE);
+        mostrarEvento(root);
+
 
         botonAñadirEvento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(botonAñadirEvento.getText().toString().equals("Añadir Evento")) {
-                    botonAñadirEvento.setText("Guardar Evento");
                     linearMostrarEvento.setVisibility(View.GONE);
                     linearAñadirEvento.setVisibility(View.VISIBLE);
-                } else {//Guardar Evento
-                    botonAñadirEvento.setText("Añadir Evento");
-                    linearMostrarEvento.setVisibility(View.VISIBLE);
-                    linearAñadirEvento.setVisibility(View.GONE);
-                }
+            }
+        });
+
+        botonGuardarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearMostrarEvento.setVisibility(View.VISIBLE);
+                linearAñadirEvento.setVisibility(View.GONE);
+                guardarEvento(root);
+                mostrarEvento(root);
+            }
+        });
+
+        botonCancelarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearMostrarEvento.setVisibility(View.VISIBLE);
+                linearAñadirEvento.setVisibility(View.GONE);
+                mostrarEvento(root);
             }
         });
 
@@ -114,6 +144,7 @@ public class CalendarioFragment extends Fragment {
                 if(month < 10){mes = "0" + month; }
                 if(dayOfMonth < 10){dia = "0" + dayOfMonth;}
                 String fecha = dia + "/" + mes + "/" + year;
+                diaActual = dia;mesActual = mes;añoActual = año.toString();
                 miFecha.setText(fecha);
 
             }
@@ -152,6 +183,45 @@ public class CalendarioFragment extends Fragment {
         Animation efectoAnimacionDerecha = AnimationUtils.loadAnimation(getContext(), R.anim.animacion_desde_derecha);
         view.startAnimation(efectoAnimacionDerecha);
 
+    }
+
+    public void guardarEvento(View root) {
+        SharedPreferences datos = getContext().getSharedPreferences("Datos", Context.MODE_PRIVATE);
+        Set<String> listaEventos = new HashSet<>();
+        listaEventos = datos.getStringSet("listaEventos", new HashSet<>());
+        SharedPreferences.Editor editar = datos.edit();
+        EditText titulo = root.findViewById(R.id.editTituloCalendar);
+        EditText descripcion = root.findViewById(R.id.editDescripcionCalendario);
+        Button hora = root.findViewById(R.id.botonSeleccionarHora);
+        //Lo almacenamos en un unico string que luego parsearemos (es cutre Paco, lo se :( )
+        String evento = diaActual + "/" + mesActual + "/" + añoActual + "&" + titulo.getText().toString() + "&"
+                + descripcion.getText().toString() + "&" + hora.getText().toString() + "&";
+        listaEventos.add(evento);
+        editar.putStringSet("listaEventos", listaEventos);
+        editar.commit();
+    }
+
+    public void mostrarEvento(View root) {
+        listViewCalendario = root.findViewById(R.id.lista_eventos);
+        SharedPreferences datos = getContext().getSharedPreferences("Datos", Context.MODE_PRIVATE);
+        Set<String> listaEventos = new HashSet<>();
+        listaEventos = datos.getStringSet("listaEventos", new HashSet<>());
+        //Hay que filtrar para mostrar solo los eventos de ese dia
+        ArrayList listaEventosArray = filtrarEventosPorFecha(listaEventos);
+        ArrayAdapter listaEventosAdapter = new ListaCalendario(getContext(), listaEventosArray);
+        listViewCalendario.setAdapter(listaEventosAdapter);
+
+    }
+
+    public ArrayList<String> filtrarEventosPorFecha(Set<String> listaEventosSet) {
+        ArrayList<String> listaEventos = new ArrayList<>(listaEventosSet);
+        String fecha = diaActual + "/" + mesActual + "/" + añoActual;
+        for (int i = 0; i < listaEventos.size(); i++) {
+            if(!fecha.equals(listaEventos.get(i).substring(0, 10))) {//Si no es la fecha seleccionamos la borramos
+                listaEventos.remove(i);
+            }
+        }
+        return listaEventos;
     }
 
 }
---
+++ app/src/main/java/com/project/getfit/ui/recetas/ListaCalendario.java
@@ -0,0 +1,57 @@
+package com.project.getfit.ui.recetas;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.project.getfit.R;
+
+import java.util.ArrayList;
+
+public class ListaCalendario extends ArrayAdapter<String> {
+
+    public ListaCalendario(Context c, ArrayList<String> records) {
+
+        super(c, 0, records);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final String item= getItem(position);
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.lista_calendario, null);
+        }
+        TextView textoTitulo = convertView.findViewById(R.id.textoTituloCalendario);
+        TextView textoDescripcion = convertView.findViewById(R.id.textoDescripcionCalendario);
+        TextView textoHorario = convertView.findViewById(R.id.textoHoraCalendario);
+
+        Integer indice = 0;
+        ArrayList<String> listaValores = new ArrayList<>();
+        //Vamos cogiendo los diferentes campos dentro del string
+        for(int i = 0; i < item.length(); i++) {
+            if(item.charAt(i) == '&') {
+                listaValores.add(item.substring(indice, i));
+                indice = i + 1;
+            }
+        }
+        String fecha = listaValores.get(0);
+        String titulo = listaValores.get(1);
+        String descripcion = listaValores.get(2);
+        String hora = listaValores.get(3);
+
+        textoTitulo.setText(titulo);
+        textoDescripcion.setText(descripcion);
+        textoHorario.setText(hora);
+
+        return convertView;
+    }
+
+
+}
